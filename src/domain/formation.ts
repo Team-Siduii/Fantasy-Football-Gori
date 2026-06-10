3e66c5373b72da384770c771148115f1ea9b7567
Sim Duin <s.j.m.duindam@gmail.com>
2026-06-10 13:18:03 +0000

    feat: add 3-4-3 formation

--- src/domain/formation.ts
+++ src/domain/formation.ts
@@ -11,6 +11,7 @@ const FORMATIONS: FormationConfig[] = [
   { label: "4-3-3", defenders: 4, midfielders: 3, forwards: 3 },
   { label: "4-4-2", defenders: 4, midfielders: 4, forwards: 2 },
   { label: "3-5-2", defenders: 3, midfielders: 5, forwards: 2 },
+  { label: "3-4-3", defenders: 3, midfielders: 4, forwards: 3 },
   { label: "5-3-2", defenders: 5, midfielders: 3, forwards: 2 },
 ];
 
